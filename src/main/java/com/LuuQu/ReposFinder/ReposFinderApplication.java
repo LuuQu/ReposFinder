4388bfb5f7dba57eb822991127a87a2a139ff3ae
Łukasz Wojewnik <l.wojewnik@wp.pl>
2025-12-18 14:01:14 +0100

    Initial commit

---
+++ src/main/java/com/LuuQu/ReposFinder/ReposFinderApplication.java
@@ -0,0 +1,13 @@
+package com.LuuQu.ReposFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposFinderApplication.class, args);
+	}
+
+}
